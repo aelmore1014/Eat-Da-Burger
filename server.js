15844edb8bba09c2c972fbdad68375d397a4f8f3
Alex Elmore <alexelmore1014@gmail.com>
2020-01-26 00:44:59 -0500

    final commit

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 var express = require('express');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App listening at localhost:" + PORT);
+});
