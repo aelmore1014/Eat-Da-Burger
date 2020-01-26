42c231e0524d964f57f27539f861df96d1db141c
Alex Elmore <alexelmore1014@gmail.com>
2020-01-25 23:35:59 -0500

    added an update function and a insert function

--- config/orm.js
+++ config/orm.js
@@ -35,3 +35,47 @@ cb(result);
     });
 },
 
+
+//this will insert one table entry
+insertOne: function(table, cols, vals, cb) {
+  
+    var queryString = "INSERT INTO " + table;
+
+queryString += " (";  
+queryString += cols.toString();
+queryString += ") ";
+queryString += "VALUES (";
+queryString += printQuestionMarks(vals.length);
+queryString += ") ";
+
+connection.query(queryString, vals, function(err, result) {
+        if (err) {
+            throw err;
+        }
+
+      
+    cb(result);
+    });
+},
+
+//This allows you to update a single table value
+updateOne: function(table, objColVals, condition, cb) {
+    
+    var queryString = "UPDATE " + table;
+
+queryString += " SET ";
+queryString += objToSql(objColVals);
+queryString += " WHERE ";
+queryString += condition;
+
+connection.query(queryString, function(err, result) {
+        if (err) {
+            throw err;
+        }
+
+
+cb(result);
+    });
+}
+};
+module.exports = orm;
