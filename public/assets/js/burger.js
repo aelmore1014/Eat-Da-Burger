6403a9080535c6230dd7ad06276dee8d1a38dbb5
Alex Elmore <alexelmore1014@gmail.com>
2020-03-31 17:59:09 -0400

    commit

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,42 +1,44 @@
+
+
 $(function() {
-    $(".devour").on("click", function(event) {
-      var id = $(this).data("id");
-      var newDevouredBurger = {
-        devoured: true
-      };
-  
-     
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newDevouredBurger
-      }).then(
-        function() {
-          console.log("changed devoured to", newDevouredBurger);
-         
-          location.reload();
-        }
-      );
-    });
-  
-    $(".create-form").on("submit", function(event) {
-     
-      event.preventDefault();
-  
-      var newBurger = {
-        burger_name: $("#ca").val().trim(),
-       
-      };
-  
-      
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          
-          location.reload();
-        }
-      );
-    });
+  $(".devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevouredBurger = {
+      devoured: true
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredBurger
+    }).then(
+      function() {
+        console.log("changed devoured to", newDevouredBurger);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
   });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#ca").val().trim(),
+      // devoured: $("[name=devoured]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
